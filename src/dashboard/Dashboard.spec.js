e36b15162fc9dadb53dc2fadc7549c9d2818298b
FreedomWriter <natalienetworks@gmail.com>
2019-12-04 01:21:07 -0600

    using snapshot to test controls and display show

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,15 +1,24 @@
 import React from "react";
 
 import * as rtl from "@testing-library/react";
+import { within } from "@testing-library/dom/dist/@testing-library/dom.umd.js";
 // import "@testing-library/jest-dom/extend-expect";
 
 import Dashboard from "./Dashboard";
+import Display from "../display/Display";
+import Controls from "../controls/Controls";
+import { exportAllDeclaration } from "@babel/types";
 
-jest.mock("./Dashboard", () => {
-  return;
+afterEach(rtl.cleanup);
+
+test("Dashboard renders", () => {
+  const wrapper = rtl.render(<Dashboard />);
+
+  expect(wrapper).toMatchSnapshot();
 });
 
-test("Expect default to unlocked and open", () => {
+test("Dashboard renders controls and display", () => {
   const wrapper = rtl.render(<Dashboard />);
-  expect();
+
+  expect(wrapper.asFragment).toMatchSnapshot();
 });
